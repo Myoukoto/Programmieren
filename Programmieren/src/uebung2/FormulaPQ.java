b34186ced06e044e832203e22fef48ddfb446c45
gapol <gapol@LAPTOP-0TF4JBC1.speedport.ip>
2020-03-20 02:18:40 +0100

    exercises2 commit

---
+++ Programmieren/src/uebung2/FormulaPQ.java
@@ -0,0 +1,14 @@
+package uebung2;
+
+public class FormulaPQ 
+{
+	public static void main(String[] args)
+	{
+		//Variables need to be changed here
+		int p=8;
+		int q= 4;
+		// implementation of square root and power
+		System.out.println((-p)/2+Math.sqrt(Math.pow(p/2, 2)-q));
+		System.out.println((-p)/2-Math.sqrt(Math.pow(p/2, 2)-q));
+	}
+}
